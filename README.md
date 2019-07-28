c1fabd1d304818a30b824976a89a78dfd9d412e5
arijit sahoo <tijira1999sahoo@gmail.com>
2019-07-28 22:36:25 +0530

    added auth

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Rest_API
 simple REST API
 
-just starting to learn about restful api, express js
+just starting to learn about restful api, express js,mongodb using mongoose package and mlab
--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// middlewares
+// a function that executes when routes are hit
+// we can restrict this
+
+// app.use('/posts', () =>{
+//     console.log("this is a middleware running"); //this message will be printed on the console when we run the localhost:3000/posts that is when we hit the posts route
+
+    
+// });
+
+// app.use(auth); this will check whether a particular user is authenticated for any specific posts
+
 
 // create routes now
 app.get('/',(req,res) => {
